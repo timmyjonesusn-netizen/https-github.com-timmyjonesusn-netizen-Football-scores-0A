a7de4bd7aa540b3b75b101cda558b8e5090f387a
Timmyart <timmyjonesusn@gmail.com>
2025-10-21 18:56:48 -0500

    Update serv_timmyapp.py

--- serv_timmyapp.py
+++ serv_timmyapp.py
@@ -2,14 +2,38 @@
 from flask import Flask, render_template
 
 def create_app():
-    app = Flask(__name__, static_folder="static", template_folder="templates")
+    app = Flask(__name__, static_folder="static", static_url_path="/static")
 
     @app.route("/")
-    def index():
-        return render_template("index.html")
+    def home():
+        return render_template("index.html", page_title="Welcome to TimmyApp")
+
+    @app.route("/ideas")
+    def ideas():
+        return render_template("stub.html", page_title="Ideas")
+
+    @app.route("/bubbles")
+    def bubbles():
+        return render_template("stub.html", page_title="Timmy Bubbles")
 
     @app.route("/music")
     def music():
-        return render_template("music.html")
+        return render_template("music.html", page_title="TimmyTunes")
+
+    @app.route("/police")
+    def police():
+        return render_template("stub.html", page_title="Police Corner")
+
+    @app.route("/tickle")i
+    def tickle():
+        return render_template("stub.html", page_title="Daily Tickle")
+
+    @app.route("/riddles")
+    def riddles():
+        return render_template("stub.html", page_title="Riddles")
+
+    @app.route("/weather")
+    def weather():
+        return render_template("stub.html", page_title="Weather")
 
     return app
