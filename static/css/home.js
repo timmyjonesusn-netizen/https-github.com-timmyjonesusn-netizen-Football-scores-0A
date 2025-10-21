29d2ab25d749fcd381cf752abbac5600133979e7
Timmyart <timmyjonesusn@gmail.com>
2025-10-21 17:34:33 -0500

    Update home.js

--- static/css/home.js
+++ static/css/home.js
@@ -1,1 +1,5 @@
+// Gentle background glow effect for extra life
+setInterval(() => {
+  document.body.style.background = `radial-gradient(circle at ${Math.random()*100}% ${Math.random()*100}%, #2b0040, #000)`;
+}, 8000);
 
