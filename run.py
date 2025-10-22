445b84627a9a003a167156b9a4e35013976a351a
Timmyart <timmyjonesusn@gmail.com>
2025-10-22 12:53:19 -0500

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-# run.py
-from serv_timmyapp import create_app
-
-if __name__ == "__main__":
-    app = create_app()
-    # 0.0.0.0 so it works on Render/local; port 8000 default
-    app.run(host="0.0.0.0", port=8000, debug=True)
