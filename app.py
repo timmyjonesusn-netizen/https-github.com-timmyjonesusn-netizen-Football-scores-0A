b3e2767ed1237a80984d26ce4dd80a5b3014e6ae
Timmyart <timmyjonesusn@gmail.com>
2025-10-23 14:51:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return render_template("index.html")
+
+@app.get("/healthz")
+def healthz():
+    return {"status": "ok"}
+
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
