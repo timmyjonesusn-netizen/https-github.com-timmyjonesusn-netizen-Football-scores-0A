026e0a79d346c4da98cd8f55d1861336e7d5c6c0
Timmyart <timmyjonesusn@gmail.com>
2025-10-23 15:43:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-
-    @app.get("/")
-    def index():
-        return "TimmyApp is alive ✨"
-
-    return app
+@app.get("/")
+def index():
+    return "TimmyApp is alive ✨"
