7f95c8a4e558e49d7e1b864dec0064c4564596ef
Timmyart <timmyjonesusn@gmail.com>
2025-10-22 22:02:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.get("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.get("/healthz")
+def healthz():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
