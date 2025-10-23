c6235376dfe516e7510f56b1b29230e3d9f5f137
Timmyart <timmyjonesusn@gmail.com>
2025-10-23 18:42:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify
-
 app = Flask(__name__)
 
 @app.get("/")
 def home():
-    # ultra-simple payload so you KNOW it's your app
     return jsonify(status="ok", app="TimmyApp", message="Comms up 💜")
 
 @app.get("/healthz")
