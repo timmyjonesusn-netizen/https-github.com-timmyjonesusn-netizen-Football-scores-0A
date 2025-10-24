53886e0657ee7c419d00549b219bd97972738921
Timmyart <timmyjonesusn@gmail.com>
2025-10-23 20:45:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+# app.py (repo root)
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.get("/")
 def home():
-    # If you don't have templates/index.html yet, use: return "Hello Timmy!"
+    # if you don't have templates/index.html yet, use the plain string:
+    # return "Hello Timmy!"
     return render_template("index.html")
