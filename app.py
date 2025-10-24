05079a6940f9303740ee8e0410631f6747ab9695
Timmyart <timmyjonesusn@gmail.com>
2025-10-23 20:42:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.get("/")
 def home():
-    # if templates/index.html isn't present yet, use: return "Hello Timmy!"
+    # If you don't have templates/index.html yet, use: return "Hello Timmy!"
     return render_template("index.html")
