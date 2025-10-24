7a47e25fc4fdd07e4670ae2ee5a6b0959d8cc90b
Timmyart <timmyjonesusn@gmail.com>
2025-10-23 21:33:52 -0500

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)          # <-- MUST exist at module top level
-
-@app.get("/")
-def index():
-    return "hello"
-
-if __name__ == "__main__":
-    app.run()
