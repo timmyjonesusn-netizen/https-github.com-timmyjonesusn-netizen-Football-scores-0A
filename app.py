40bdc2aa44b3661f05f9a4a96d1433c735538d2e
Timmyart <timmyjonesusn@gmail.com>
2025-10-23 19:59:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-from app import create_app   # <-- not app_core
-app = create_app()
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")  # or just "Hello, Timmy!"
+
+    return app   # <-- THIS LINE IS REQUIRED
