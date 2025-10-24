c9aa4e1b93562dad9b89fc578e244c3fa4dae568
Timmyart <timmyjonesusn@gmail.com>
2025-10-23 20:36:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-# app.py (repo root)
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.get("/")
 def home():
-    # if templates/index.html isn't there yet, just return a string
+    # if templates/index.html isn't present yet, use: return "Hello Timmy!"
     return render_template("index.html")
-    # or: return "Hello Timmy!"
